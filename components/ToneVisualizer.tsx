9ad7c0bd1b91eda1336200f9ca0cc1b6741a35eb
bhawnaa05 <bhawnabhandari2004@gmail.com>
2026-01-07 18:07:01 +0530

    Fix production build and visualizer props

--- components/ToneVisualizer.tsx
+++ components/ToneVisualizer.tsx
@@ -2,11 +2,18 @@
 
 import CircularVisualizer from './CircularVisualizer';
 
+import { useAudioContext } from '../hooks/useAudioContext';
+
 export default function ToneVisualizer() {
+  const { isInitialized, startPolling } = useAudioContext();
+
   return (
     <div className="p-4">
       <h2 className="text-lg font-medium text-cyan-300 mb-3">Tone Visualizer</h2>
-      <CircularVisualizer />
+      <CircularVisualizer
+        isInitialized={isInitialized}
+        startPolling={startPolling}
+      />
     </div>
   );
 }
